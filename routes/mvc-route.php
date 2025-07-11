1d20a87703ba5c31907419ade796cd4a0b1f1b62
unknown <farhan21998@gmail.com>
2025-07-11 09:33:51 +0700

    fixing bugs

--- routes/mvc-route.php
+++ routes/mvc-route.php
@@ -2,13 +2,6 @@
 use Illuminate\Support\Facades\Route;
 Route::group(['prefix'=>config('mvc.route_prefix')], function () { // remove this line if you dont have route group prefix
     Route::group(['middleware'=>['userRoles']], function () {
-		//profile-pimpinan
-		Route::prefix('profile-pimpinan')->as('profile-pimpinan')->group(function () {
-			Route::get('data', 'ProfilePimpinan\ProfilePimpinanController@data');
-			Route::get('delete/{id}', 'ProfilePimpinan\ProfilePimpinanController@delete');
-		});
-		Route::resource('profile-pimpinan', 'ProfilePimpinan\ProfilePimpinanController');
-		//end-profile-pimpinan
 		//profile-instansi
 		Route::prefix('profile-instansi')->as('profile-instansi')->group(function () {
 			Route::get('data', 'ProfileInstansi\ProfileInstansiController@data');
